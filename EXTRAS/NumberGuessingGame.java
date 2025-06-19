635e3783835b205f6311c47f51a77f351dcf84a1
ADITYA SHARMA <114454863+AD9I@users.noreply.github.com>
2025-06-19 11:50:36 +0530

    [Aditya Sharma ] Add  Number Guessing Game

--- EXTRAS/NumberGuessingGame.java
+++ EXTRAS/NumberGuessingGame.java
@@ -1,17 +1,27 @@
-import java.util.Scanner;
-import java.util.Random;
-public class NumberGuessingGame {
+import java.util.*;
+
+public class Game {
+    static Scanner sc = new Scanner(System.in);
+    static Random r = new Random();
+
+    static int guess(int l, int h) {
+        return r.nextInt(h - l + 1) + l;
+    }
+
+    static String feedback(int g) {
+        System.out.println("Is your number " + g + "? (high/low/correct)");
+        return sc.nextLine().toLowerCase();
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
-        int low = 1, high = 100;
+        int l = 1, h = 100;
         while (true) {
-            int guess = (low + high) / 2;
-            System.out.println("Is your number " + guess + "? (high/low/correct)");
-            String feedback = sc.next();
-            if (feedback.equals("correct")) break;
-            else if (feedback.equals("high")) high = guess - 1;
-            else if (feedback.equals("low")) low = guess + 1;
+            int g = guess(l, h);
+            String f = feedback(g);
+            if (f.equals("correct")) break;
+            if (f.equals("high")) h = g - 1;
+            else if (f.equals("low")) l = g + 1;
         }
+        System.out.println("Yay! Guessed it 🎉");
     }
 }
