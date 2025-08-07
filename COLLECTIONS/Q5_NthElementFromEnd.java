c0c8b7b0d2d1878212e93f7bbb6f0ea0f0bb4cda
ADITYA SHARMA <114454863+AD9I@users.noreply.github.com>
2025-08-07 14:13:25 +0530

    [Aditya Sharma] Add Nth Element From End

---
+++ COLLECTIONS/Q5_NthElementFromEnd.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Q5_NthElementFromEnd {
+    public static <T> T findNthFromEnd(LinkedList<T> list, int n) {
+        Iterator<T> fast = list.iterator();
+        Iterator<T> slow = list.iterator();
+
+        for (int i = 0; i < n; i++) {
+            if (fast.hasNext()) {
+                fast.next();
+            } else {
+                throw new IllegalArgumentException("N is larger than the list size");
+            }
+        }
+
+        while (fast.hasNext()) {
+            fast.next();
+            slow.next();
+        }
+
+        return slow.next();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>(Arrays.asList("A", "B", "C", "D", "E"));
+        int n = 2;
+        System.out.println("Nth element from the end: " + findNthFromEnd(list, n));
+    }
+}
